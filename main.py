62dcd31bdca8c8e93d4fae3e5f4eeba6cf1d190e
kathleens_13 <kathleen.a.schenck@gmail.com>
2025-04-14 10:56:17 -0400

    added sorting report and moby dick and pride and prejudice to books

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
@@ -5,12 +6,26 @@ def get_book_text(path_to_file):
 
 from stats import get_num_words
 from stats import get_num_character
+from stats import sort_for_report
 
 def main():
-    num_words = get_num_words(get_book_text("books/frankenstein.txt"))
-    print(f"{num_words} words found in the document")
-    num_character_dict = get_num_character(get_book_text("books/frankenstein.txt"))
-    print(num_character_dict)
+    if len(sys.argv) <= 1:
+        print("Usage: python3 main.py <path_to_book>")
+        return sys.exit(1)
+    else:
+        num_words = get_num_words(get_book_text(sys.argv[1]))
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {sys.argv[1]}...")
+        print("----------- Word Count ----------")
+        print(f"Found {num_words} total words")
+        num_character_dict = get_num_character(get_book_text(sys.argv[1]))
+        sorted_dict = sort_for_report(num_character_dict)
+        print("--------- Character Count -------")
+        for char_dict in sorted_dict:
+            char = char_dict["char"]
+            if char.isalpha():
+                print(f"{char}: {char_dict['count']}")
+        print("============= END ===============")
 
 
 main()
--- stats.py
+++ stats.py
@@ -3,6 +3,7 @@ def get_num_words(book_as_string):
     total_words = len(list_words)
     return total_words
 
+# get_num_character changes book_as_string into dictionary Letter : count
 def get_num_character(book_as_string):
     char_dict = {}
     char_make_lower = book_as_string.lower()
@@ -14,3 +15,16 @@ def get_num_character(book_as_string):
             char_dict[char] = 1
     return char_dict
 
+def sort_for_report(char_dict):
+    #first change char_dict into dictionary w/two key-value pairs
+    result_list=[]
+    for character, count in char_dict.items():
+        #create new dictionary for each character with 'char' and 'count' keys
+        new_char_dict = {"char": character, "count": count}
+        result_list.append(new_char_dict)
+    #create function to tell sort which key to use
+    def sort_using(dict_item):
+        return dict_item["count"]
+    #sort the list in place
+    result_list.sort(reverse=True, key=sort_using)
+    return result_list
